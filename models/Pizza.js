b8fe34fe49076dd259a147b8114248b1e7ef3f19
dimas <dprcontractor@gmail.com>
2022-04-21 18:34:25 -0400

    set up comments , format date with getter

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose')
+const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
 
@@ -12,7 +13,8 @@ const PizzaSchema = new Schema({
 
         createdAt: {
             type: Date,
-            default: Date.now
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
         },
 
         size: {
@@ -20,8 +22,29 @@ const PizzaSchema = new Schema({
             default: 'Large'
         },
         
-        toppings: []
-    
+        toppings: [],
+
+        comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+}, 
+
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 })
 
 //create pizza model using the PizzaSchema
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
+const commentRoute = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
 //add prefix of `/pizzas` to routes created in `pizza-routes.js`
-
+router.use('/comments', commentRoute);
 router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
