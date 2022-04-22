79aa5df709369836ac9933c078676fc96a905509
dimas <dprcontractor@gmail.com>
2022-04-22 16:51:02 -0400

    add-replies

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,6 +1,12 @@
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+} = require('../../controllers/comment-controller');
 
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
@@ -9,5 +15,12 @@ router.route('/:pizzaId').post(addComment);
 // /api/comments/<pizzaId>/<commentId>
 router.route('/:pizzaId/:commentId').delete(removeComment);
 
+//edit existing comment
+router
+.route('/:pizzaId/commentId')
+.put(addReply)
+.delete(removeComment)
 
+//DELETE reply
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply)
 module.exports = router;
